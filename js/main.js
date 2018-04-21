5bc3703ae96487ca164196560e404363754ca3cf
Brian Katz <katzbr@oregonstate.edu>
2018-04-20 22:55:47 -0700

    updated repository

--- js/main.js
+++ js/main.js
@@ -1,102 +1,167 @@
-var mymap = L.map('map', {center: [44.13, -119.93], zoom: 7});
+var hideLabel = function(label){ label.labelObject.style.opacity = 0;};
+var showLabel = function(label){ label.labelObject.style.opacity = 1;};
+var labelEngine = new labelgun.default(hideLabel, showLabel);
 
-L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-    maxZoom: 11,
-    minZoom: 6,
-    detectRetina: true, //support Retina Display if the client uses high resolution monitor.
-    attribution: 'Cell Tower Data &copy; Map Cruzin | Oregon counties &copy; Oregon Explorer | Base Map &copy; CartoDB'
-}).addTo(mymap);
-
-
-//https://github.com/gka/chroma.js/blob/master/src/colors/colorbrewer.coffee
-var colors = chroma.scale('Accent').mode('lch').colors(9);
-
-for (i = 0; i < 9; i++) {
-    $('head').append($("<style> .marker-color-" + (i + 1).toString() + " { color: " + colors[i] + "; font-size: 15px; text-shadow: 0 0 3px #ffffff;} </style>"));
+var labels = [];
+// Alber's Equal Area Projection
+var crs = new L.Proj.CRS('EPSG:3085', '+proj=aea +lat_1=27.5 +lat_2=35 +lat_0=18 +lon_0=-100 +x_0=1500000 +y_0=6000000 +ellps=GRS80 +units=m +no_defs', {
+    origin: [416729.617118, 412156.942963],
+    bounds: [
+        [-2410329.835748, 13545805.10653],
+        [-10850237.174162, 3977377.378424]
+    ],
+    resolutions: [
+        4891.96999883583 * 8,
+        4891.96999883583 * 4,
+        4891.96999883583 * 2,
+        4891.96999883583,
+        2445.98499994708,
+        1222.99250010583,
+        611.496250052917,
+        305.748124894166,
+        152.8740625,
+        76.4370312632292,
+        38.2185156316146,
+        19.1092578131615,
+        9.55462890525781,
+        4.77731445262891,
+        2.38865722657904,
+        1.19432861315723,
+        0.597164306578613,
+        0.298582153289307,
+        0.298582153289307 / 2,
+        0.298582153289307 / 4,
+        0.298582153289307 / 8,
+        0.298582153289307 / 16
+    ]
+});
+// define map object and parameters
+var mymap = L.map('map', {
+    center: [38, -98],
+    zoom: 3,
+    maxZoom: 10,
+    minZoom: 2,
+    detectRetina: true,
+    crs: crs // set projection to Alber's Equal Area
+});
+// define color scale for airport markers (color-blind friendly yellow/purple combo)
+var colors = chroma.scale(['#ffff00', '#800080']).mode('lch').colors(2);
+// set color of airport markers based on control tower Y/N (id 1/0)
+for (i = 0; i < 2; i++) {
+    $('head').append($("<style> .marker-color-" + (i + 1).toString() + " { color: " + colors[i] + "; font-size: 14px; text-shadow: 1px 2px 6px #585858;} </style>"));
 }
-
-
-// Get GeoJSON and put on it on the map when it loads
-L.geoJson.ajax("assets/cell_towers.geojson", {
+// add airports data layer
+airports = L.geoJson.ajax("assets/airports.geojson", {
     onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.company);
-    },
+        layer.bindPopup(feature.properties.AIRPT_NAME)},
     pointToLayer: function (feature, latlng) {
         var id = 0;
-        if (feature.properties.company == "New Cingular") { id = 0; }
-        else if (feature.properties.company == "Cellco")  { id = 1; }
-        else if (feature.properties.company == "RCC Minnesota")  { id = 2; }
-        else if (feature.properties.company == "Verizon")  { id = 3; }
-        else if (feature.properties.company == "US Cellular")  { id = 4; }
-        else if (feature.properties.company == "Hood River Cellular")  { id = 5; }
-        else if (feature.properties.company == "Medford Cellular")  { id = 6; }
-        else if (feature.properties.company == "Oregon RSA")  { id = 7; }
-        else { id = 8;} // "Salem Cellular"
-        return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-signal marker-color-' + (id + 1).toString() })});
-    }
-}).addTo(mymap);
-
-//https://github.com/gka/chroma.js/blob/master/src/colors/colorbrewer.coffee
-// Set function for color ramp
-colors = chroma.scale('OrRd').mode('hsl').colors(5); //colors = chroma.scale('OrRd').colors(5);
+        if (feature.properties.CNTL_TWR === "Y") { id = 0; }
+        else { id = 1;} // "N"
+        return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-plane marker-color-' + (id + 1).toString() })});
+    }}).addTo(mymap);
+// set color scale for us-states from purple to yellow (color-blind friendly)
+colors = chroma.scale(['#800080', '#ffff00']).mode('lch').colors(5);
+// set color scale for legend break points
 function setColor(density) {
     var id = 0;
-    if (density > 18) { id = 4; }
-    else if (density > 13 && density <= 18) { id = 3; }
-    else if (density > 10 && density <= 13) { id = 2; }
+    if (density > 20) { id = 4; }
+    else if (density > 15 && density <= 20) { id = 3; }
+    else if (density > 10 && density <= 15) { id = 2; }
     else if (density > 5 &&  density <= 10) { id = 1; }
     else  { id = 0; }
     return colors[id];
 }
-
-// Set style function that sets fill color property equal to cell tower density
+// set choropleth fill color based on airport count
 function style(feature) {
     return {
-        fillColor: setColor(feature.properties.CT_CNT),
-        fillOpacity: 0.4,
+        fillColor: setColor(feature.properties.count),
+        fillOpacity: 0.2,
         weight: 2,
         opacity: 1,
         color: '#ffffff',
-        dashArray: '4'
+        dashArray: '3'
     };
 }
-
-// Add Neighborhood Polygons
-L.geoJson.ajax("assets/counties.geojson", {
-    style: style
+// add us-states data layer
+states = L.geoJson.ajax("assets/us-states.geojson", {
+    style: style,
+    onEachFeature: function (feature, label) {
+        label.bindTooltip(feature.properties.name, {
+            className: 'feature-label',
+            permanent: true,
+            direction: 'center'
+        });
+        labels.push(label);
+    }
 }).addTo(mymap);
-
-
-// Create Leaflet Control Object for Legend
+// define legend
 var legend = L.control({position: 'topright'});
-
-// Function that runs when legend is added to map
+// set legend color scale and breaks
 legend.onAdd = function () {
-
-    // Create Div Element and Populate it with HTML
     var div = L.DomUtil.create('div', 'legend');
-    div.innerHTML += '<b># Cell Tower</b><br />';
-    div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>19+</p>';
-    div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>14-18</p>';
-    div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>11-13</p>';
-    div.innerHTML += '<i style="background: ' + colors[1] + '; opacity: 0.5"></i><p> 6-10</p>';
+    div.innerHTML += '<b># Airports/State</b><br />';
+    div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>20+</p>';
+    div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>15-20</p>';
+    div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>10-15</p>';
+    div.innerHTML += '<i style="background: ' + colors[1] + '; opacity: 0.5"></i><p> 5-10</p>';
     div.innerHTML += '<i style="background: ' + colors[0] + '; opacity: 0.5"></i><p> 0- 5</p>';
-    div.innerHTML += '<hr><b>Company<b><br />';
-    div.innerHTML += '<i class="fa fa-signal marker-color-1"></i><p> New Cingular</p>';
-    div.innerHTML += '<i class="fa fa-signal marker-color-2"></i><p> Cello</p>';
-    div.innerHTML += '<i class="fa fa-signal marker-color-3"></i><p> RCC Minnesota</p>';
-    div.innerHTML += '<i class="fa fa-signal marker-color-4"></i><p> Verizon</p>';
-    div.innerHTML += '<i class="fa fa-signal marker-color-5"></i><p> US Cellular</p>';
-    div.innerHTML += '<i class="fa fa-signal marker-color-6"></i><p> Hood River Cellular</p>';
-    div.innerHTML += '<i class="fa fa-signal marker-color-7"></i><p> Medford Cellular</p>';
-    div.innerHTML += '<i class="fa fa-signal marker-color-8"></i><p> Oregon RSA</p>';
-    div.innerHTML += '<i class="fa fa-signal marker-color-9"></i><p> Salem Cellular</p>';
-    // Return the Legend div containing the HTML content
+    div.innerHTML += '<hr><b>Control Tower Present<b><br />';
+    div.innerHTML += '<i class="fa fa-plane marker-color-1"></i><p> Yes</p>';
+    div.innerHTML += '<i class="fa fa-plane marker-color-2"></i><p> No</p>';
     return div;
 };
 
-// Add a legend to map
 legend.addTo(mymap);
 
-// Add a scale bar to map
-L.control.scale({position: 'bottomleft'}).addTo(mymap);
+// https://github.com/cloudybay/leaflet.latlng-graticule
+L.latlngGraticule({
+    showLabel: true,
+    opacity: 0.2,
+    color: "#000000",
+    fillColor: '#747474',
+    zoomInterval: [
+        {start: 3, end: 3.9, interval: 4},
+        {start: 4, end: 5.9, interval: 2},
+        {start: 6, end: 7.9, interval: 0.5},
+        {start: 8, end: 10, interval: 0.25}
+    ]
+}).addTo(mymap);
+
+function addLabel(layer, id) {
+    var label = layer.getTooltip()._source._tooltip._container;
+    if (label) {
+        // We need the bounding rectangle of the label itself
+        var rect = label.getBoundingClientRect();
+        // We convert the container coordinates (screen space) to Lat/lng
+        var bottomLeft = mymap.containerPointToLatLng([rect.left, rect.bottom]);
+        var topRight = mymap.containerPointToLatLng([rect.right, rect.top]);
+        var boundingBox = {
+            bottomLeft : [bottomLeft.lng, bottomLeft.lat],
+            topRight   : [topRight.lng, topRight.lat]
+        };
+        // Ingest the label into labelgun itself
+        labelEngine.ingestLabel(
+            boundingBox,
+            id,
+            parseInt(Math.random() * (5 - 1) + 1), // Weight
+            label,
+            label.innerText,
+            false
+        );
+        // If the label hasn't been added to the map already
+        // add it and set the added flag to true
+        if (!layer.added) {
+            layer.addTo(mymap);
+            layer.added = true;
+        }
+    }
+}
+
+mymap.on("zoomend", function(){
+    var i = 0;
+    states.eachLayer(function(label){
+        addLabel(label, ++i);
+    });
+    labelEngine.update();
+});
--- readme.md
+++ readme.md
@@ -1,11 +1,50 @@
+###Grading Rubric:
+
+Control tower icons. **(3 points)**
+
+Choropleth map based on the number of airports within each state. **(3 points)**
+
+This web map of airports needs to include:
+
+- An appropriate projection - describe that projection in the `readme.md` page; **(7 points)**
+- Graticules - change the default color, font, and build your own version of intervals; **(5 points)**
+- Dynamic labels of the state name - use a font different from the font of the legend; **(6 points)**
+- Some interactive elements, like a clickable marker; **(3 points)**
+- Legend; **(5 points)**
+
+Your interactive web map should be accessible from a url link, which should be `http://[your_github_username].github.io/[your_repository_name]/index.html`; **(6 points)**
+
+Put the javascript code into a separate javascript file `main.js` under the `js` folder, and put the css code into a separate css file `main.css` under the `css` folder. If you will use any images or videos, please put those in the `img` folder. The geojson data should remain in the `assets` folder. Please make sure the project repository structure is similar to the file structure below. **(4 points)**
+
+```
+[your_repository_name]
+    │index.html
+    │readme.md
+    ├─assets
+    │      airports.geojson
+    │      us-states.geojson
+    ├─css
+    │      main.css
+    ├─img
+    │      xxx.jpg
+    └─js
+            main.js
+```
+
+Write up a project description in the `readme.md` file. **(8 points)**
+
 **Project Name:** Airports in the United States
 
 **Introduction:** This map was created to display all of the airports in the United States and whether or not those airports have control towers.
 
+**Projection:**
+
 **Major Functions:** This map can assist pilots that wish to see what airports they are nearby to and which ones will have control towers that can communicate with their airplanes. A video of an airplane taking off was included for an extra eye-catching effect, and users of this web map can pause or play the video using the buttons on the right side of the screen.
+
+**Libraries:**
 
 **Data Sources:** The dataset containing the list of airports in the United States was obtained from USGS, here: https://catalog.data.gov/dataset/usgs-small-scale-dataset-airports-of-the-united-states-201207-shapefile. The dataset containing the state boundaries of the United States was obtained from Mike Bostock of D3, here: https://bost.ocks.org/mike/
 
 **Credits:** This map was created with Leaflet, data from USGS and D3, and CartoDB.
 
-**Acknowledgements:** I'd like to thank professor Bo Zhao at Oregon State University for his web mapping expertise and guidance. I highly recommend his GEOG 371 Web Mapping course.
+**Acknowledgements:** I'd like to thank professor Bo Zhao at Oregon State University for his web mapping expertise and guidance. I highly recommend his GEOG 371 Web Mapping and GEOG 572 Geovisual Analytics courses.
